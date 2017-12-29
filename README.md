5ac2375e016fb23df5cb942167b7a288d18d83aa
paulodacaya <paulodacaya@gmail.com>
2017-12-30 00:04:46 +1100

    initlal commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+### yu-gi-oh duel calculator
+An application that makes it easier for yu-gi-oh dueling, no need to get your calculators or do some math when you have this application!
---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//CSS
+import '../css/App.css';
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        yu-gi-oh
+      </div>
+    );
+  }
+}
+
