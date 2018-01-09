0a5b67fe8da96f853cf78483a409d22efac42526
paulodacaya <paulodacaya@gmail.com>
2018-01-09 19:27:20 +1100

    added stypes to calculator.

--- src/components/Duel/DuelCalculator.js
+++ src/components/Duel/DuelCalculator.js
@@ -12,9 +12,9 @@ const customModalStyles = {
     bottom: "",
     left: "",
     transform: "translate(50%, -50%)",
-    padding: "0",
     borderRadius: "0.3em",
     border: "none",
+    padding: "0",
   }
 }
 
@@ -22,48 +22,60 @@ export default class DuelCalculator extends Component {
 
   state = {
     modalIsOpen: false,
+    calculation: "",
   }
 
   openModal = () => {
-    this.setState({
-      modalIsOpen: true,
+    this.setState({ 
+      modalIsOpen: true 
     })
   }
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false })
+  }
+
+
 
   render() {
     return (
       <Fragment>
-        <div className="lp-btns">
-          <button onClick={this.openModal}>direct lifepoints</button>
-          <button>calculate</button>
-        </div>
+        <button className="lp-btn" onClick={this.openModal}>calculate</button>
 
         <Modal
+          ariaHideApp={false}
           isOpen={this.state.modalIsOpen}
           style={customModalStyles} >
 
-          <form id="calculator">
-            <input type="text" name="input" placeholder="read 'how to use app' for more info"/>
-            <div class="buttons">
-              <button type="button" value="+">+</button>
-              <button type="button" value="-">-</button>
-              <button type="button" value="*">X</button>
-              <button type="button" value="/">/</button>
-              <button type="button" value="(">(</button>
-              <button type="button" value=")">)</button>
-              <button type="button" value="7">7</button>
-              <button type="button" value="8">8</button>
-              <button type="button" value="9">9</button>
-              <button type="button" value="4">4</button>
-              <button type="button" value="5">5</button>
-              <button type="button" value="6">6</button>
-              <button type="button" value="1">1</button>
-              <button type="button" value="2">2</button>
-              <button type="button" value="3">3</button>
-              <button type="button" value="0">0</button>
-              <button type="button" value="c">)</button>
-              <button type="button" value="del">del</button>
-              <button type="submit" value="submit">OK</button>
+          <form id="calculator" name="calculator">
+            <input 
+              readOnly
+              type="text" 
+              name="input" 
+              placeholder="Check guide for more info" />
+
+            <button className="close" onClick="this.closeModal">X</button>
+            <div className="buttons">
+              <button className="sym" type="button" value="-">-</button>
+              <button className="sym" type="button" value="+">+</button>
+              <button className="sym" type="button" value="*">X</button>
+              <button className="sym" type="button" value="/">/</button>
+              <button className="num" type="button" value="7">7</button>
+              <button className="num" type="button" value="8">8</button>
+              <button className="num" type="button" value="9">9</button>
+              <button className="sym" type="button" value="(">(</button>
+              <button className="num" type="button" value="4">4</button>
+              <button className="num" type="button" value="5">5</button>
+              <button className="num" type="button" value="6">6</button>
+              <button className="sym" type="button" value=")">)</button>
+              <button className="num" type="button" value="1">1</button>
+              <button className="num" type="button" value="2">2</button>
+              <button className="num" type="button" value="3">3</button>
+              <button type="submit" value="submit"></button>
+              <button className="num" type="button" value="0">0</button>
+              <button className="num" type="button" value=".">.</button>
+              <button className="num" type="button" value="del">del</button>
+              <button className="sub" type="submit" value="submit">OK</button>
             </div>
           </form>
         </Modal>
