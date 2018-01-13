42cc3651abc073ae6785aab993f1950feb10f010
paulodacaya <paulodacaya@gmail.com>
2018-01-13 13:25:01 +1100

    Fixed typo and improved CSS.

--- src/components/Duel/AppGuide.js
+++ src/components/Duel/AppGuide.js
@@ -8,7 +8,7 @@ const AppGuide = () => (
         way to duel rather using calculators or pen/paper to 
         keep track of battle.
       </h2>
-
+      
       <ul>
         <li><span>Dueling:</span> The dueling scoreboard that keeps track of life points,
         player's name and dynamic calculations.</li>
@@ -18,7 +18,7 @@ const AppGuide = () => (
       <ul>
         <li><span>undo recent calculations:</span> Click on undo button that will undo the most recent calculation, cannot undo further calculations.</li>
         <li><span>Reset lifePoints:</span> Refreshing the page will reset to default. </li>
-        <li><span>change player name:</span> Double click on player name to change players name, hit 'enter' on keyboard or click 'OK'.</li>
+        <li><span>change player name:</span> Double click on player name, input anything you desire, hit 'enter' on keyboard or click 'OK'.</li>
       </ul>
       <img 
         className="gif-1"
@@ -28,19 +28,19 @@ const AppGuide = () => (
     </div>
     <div>
       <ul>
-        <li><span>'CALCULATE' button:</span> When clicked, a calculator will pop up where you can make calcualtions towards your life points. <br /><br />
-        For single digit calculations, initialize the calculation with a minus(-) or plus(+), followed by the number you wish to calculate. <br /><br />
-        Example: -500 or +800</li>
+        <li><span>'CALCULATE' button:</span> When clicked, a calculator will pop up where you can make calculations towards your life points. <br /><br />
+        For <strong>single digit calculations</strong>, initialize the calculation with a minus(-), plus(+) or multiply(*), followed by the number you wish to calculate. <br /><br />
+        <strong>Example:</strong> -500 or +800</li>
         <img 
         className="gif-2"
         src={ require('../../assets/calculate-btn.gif') } 
         alt="gif displaying basic operation of calculation." />
         <li>Though simple calculations are great, you can expand on this by writing equations where this benifits most. <br /><br />
-        At times, spells/trap/field cards and chaining can be cumbersome to deal with. This is where equation come into play. <br /><br />
-        When writing equation please initialize with minus or plus followed by parenthesis, (). Equation must follow these conditions to apply successfully. <br /><br />
-        Example: <br />
-        -(2400-1750), the difference between two monsters.<br />
-        -(2*2100+250-3000), calculate monster gains spell/field card effects.</li>
+        At times, spells/trap/field/chaining cards can be cumbersome to deal with. This is where the equation comes into play. <br /><br />
+        When writing equation <strong>please initialize with +, - or * followed by parenthesis, ()</strong>. Equations must follow these conditions to apply successfully. <br /><br />
+        <strong>Example:</strong> <br />
+        -(2400-1750), the difference between two monsters ATK damage.<br />
+        -(2*2100+250-3000), calculate effects on monster minus the ATK damage of opponent monster.</li>
         <img 
         className="gif-2"
         src={ require('../../assets/calculate-btn2.gif') } 
