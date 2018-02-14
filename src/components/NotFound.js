09624d7cf5b8ce8191f3649691d7e0ee45f5f086
paulodacaya <paulodacaya@gmail.com>
2018-02-14 19:04:23 +1100

    removed headerExistance property, single 404 page display

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,39 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const NotFound = props => {
-  if(props.headerExists) {
-    return (
-      <div className="NotFound">
-        <img  
-          src={ require('../assets/yugi.png') } 
-          alt="Yami Yugi" />
-        <div>
-          <h1><span>it's time to d-d-d-d... what!</span> I don't think I'm dueling you!</h1>
-          <h2>error code 404: page not found.</h2>
-          <p>please click on the tab 'Dueling' to duel your opponent.</p>
-        </div>    
-      </div>
-    )
-  } else {
-    return (
-      <div className="NotFound">
-        <img  
-          src={ require('../assets/yugi.png') } 
-          alt="Yami Yugi" />
-        <div>
-          <h1><span>it's time to d-d-d-d... what!</span> I don't think I'm dueling you!</h1>
-          <h2>error code 404: page not found.</h2>
-          <p><Link to="/">click here</Link> to find another duelist.</p>
-        </div>    
-      </div>
-    )
-  }
-}
-
-NotFound.propTpyes = {
-  headerExists: PropTypes.bool.isRequired,
-}
+const NotFound = () => (
+  <div className="notfound parallelogram">
+    <img
+      className="notfound__image"  
+      src={ require('../assets/warning.svg') } 
+      alt="warning design with exclamation point in the middle of a triangle" />
+    <div className="notfound__content">
+      <h2>error code 404: page not found.</h2>
+      <p><Link to="/">click here</Link> for yu-go-oh dueling app.</p>
+    </div>    
+  </div>
+);
 
 export default NotFound; 
